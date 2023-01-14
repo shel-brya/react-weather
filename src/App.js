b26d067376c732ece0709b3cfa4e5d3b1184e430
Shelby Bryant <leeshelby97@gmail.com>
2023-01-14 12:42:28 -0500

    added css styling and integrated Open Weather API

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import logo from './logo.svg';
+
 import Weather from './Weather'
-import './App.css';
+
 
 
 function App() {
   return (
-    <div className="App">
-   <div className='container'>
-    
-    <Weather />
-   </div>
+    <div>
+      <Weather />
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,45 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
+import './Weather.css';
+import axios from 'axios';
 
 export default function Weather() {
-    return (
-        <div className="Weather">
+
+    const [weatherData, setWeatherData] = useState({ ready: false });
+
+    function handleResponse(response) {
+        setWeatherData({
+            ready: true,
+            temperature: Math.round(response.data.main.temp),
+            city: response.data.name,
+            wind: response.data.wind.speed,
+            feelsLike: Math.round(response.data.main.feels_like),
+            humidity: response.data.main.humidity,
+            description: response.data.weather[0].main,
+            iconUrl: "https://ssl.gstatic.com/onebox/weather/64/cloudy.png",
+            date: "Thursday 9:30"
+        });
+
+    }
+
+
+    if (ready) {
+        return (<div className="Weather">
             <div className="container">
                 <div className="weather-border">
-                     <h1 id='city'>Bloomington</h1>
+                    <h1 id='city'>{weatherData.city}</h1>
                     <div className="d-flex weather-temperature">
-                        <img src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png" id="icon"/>
-                        <div id="temperature">37°F</div>
+                        <img src={weatherData.iconUrl} id="icon" align="left" alt="" />
+                        <h2 id="temp-display">{weatherData.temperature}°F</h2>
+
                     </div>
                     <ul>
-                        <li id="date">9:30</li>
-                        <li id="description">Cloudy</li>
+                        <li id="date">{weatherData.date}</li>
+                        <li id="description" className="text-capitalize">{weatherData.description}</li>
                     </ul>
-                    <form id="search-form" className="mb-3">
+                    <form>
                         <div className="row">
                             <div className="col-9">
-                                <input type="text" id="city-search" placeholder="Search for a new city" autocomplete="off" className="form-control"/>
+                                <input type="text" id="city-search" placeholder="Search for a new city" autocomplete="off" className="form-control" />
                             </div>
-                            <div className="col-3 p-0">
-                                <input type="submit" className="btn btn-primary" id="search-button" value="search"/>
+                            <div className="col-3">
+                                <input type="submit" className="btn btn-primary" id="search-button" value="search" />
                             </div>
                         </div>
                     </form>
 
                     <div className="percentage-table">
                         <div className="row even-space">
-                            <div class="col-2">Humidity</div>
-                            <div class="col-2">Feels like</div>
-                            <div class="col-2">Wind</div>
+                            <div className="col-2">Humidity</div>
+                            <div className="col-2">Feels like</div>
+                            <div className="col-2">Wind</div>
                         </div>
-                        <div class="row even-space">
-                            <div class="col-2"><span id="humidity"></span>15%</div>
-                            <div class="col-2"><span id="feels-like"></span>28°F</div>
-                            <div class="col-2"><span id="wind"></span>15mph</div>
+                        <div className="row even-space">
+                            <div className="col-2"><span id="humidity"></span>{weatherData.humidity}%</div>
+                            <div className="col-2"><span id="feels-like"></span>{weatherData.feelsLike}°F</div>
+                            <div className="col-2"><span id="wind"></span>{weatherData.wind} mph</div>
                         </div>
 
                     </div>
                 </div>
+                {/* <div class="weekly-border">
+                    <div class="weekly-forecast" id="forecast"></div>
+                </div> */}
+                <small class="repo">
+                    Open Source Repository
+                    <a href="https://github.com/shel-brya" target="_blank" rel="noreferrer">here</a>
+                </small>
             </div>
         </div>
-    )
+        )
+    } else {
+        const apiKey = "3c949ba49d38be2487ee278e0d2d4059";
+        let city = "Bloomington";
+        let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+        axios.get(apiUrl).then(handleResponse)
+        return ("Loading...")
+    }
+
+
 }
